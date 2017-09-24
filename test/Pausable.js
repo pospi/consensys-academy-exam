d9745c58ef6023627294f2112236e916157e95aa
pospi <pospi@spadgos.com>
2017-09-24 22:12:52 +1000

    tests for Pausable modifiers

---
+++ test/Pausable.js
@@ -0,0 +1,61 @@
+/**
+ * @package:	Consensys Academy exam pt2
+ * @author:		pospi <sam.pospi@consensys.net>
+ * @since:		2017-09-24
+ * @flow
+ */
+/* global contract before describe beforeEach assert artifacts it */
+
+const expectedExceptionPromise = require("../utils/expectedException")
+
+const Pausable = artifacts.require("./mock/PausableMock.sol")
+
+contract('Pausable', (accounts) => {
+
+	let owner0, owner1
+
+	before("should prepare", () => {
+		assert.isAtLeast(accounts.length, 2)
+		owner0 = accounts[0]
+		owner1 = accounts[1]
+	})
+
+	describe("whenPaused", () => {
+
+		it("allows actions when paused", async() => {
+			const paused = await Pausable.new(true, { from: owner0 })
+			await paused.countUpWhenPaused()
+			const callCount = await paused.counters(true)
+			assert.strictEqual(callCount.toNumber(), 1)
+		})
+
+		it("prevents actions when not paused", async() => {
+			const paused = await Pausable.new(false, { from: owner0 })
+			return expectedExceptionPromise(
+				() => paused.countUpWhenPaused({ from: owner0, gas: 3000000 }),
+				3000000
+			)
+		})
+
+	})
+
+	describe("whenNotPaused", () => {
+
+		it("allows actions when not paused", async() => {
+			const paused = await Pausable.new(false, { from: owner0 })
+			await paused.countUpWhenNotPaused()
+			const callCount = await paused.counters(false)
+			assert.strictEqual(callCount.toNumber(), 1)
+		})
+
+		it("prevents actions when paused", async() => {
+			const paused = await Pausable.new(true, { from: owner0 })
+			return expectedExceptionPromise(
+				() => paused.countUpWhenNotPaused({ from: owner0, gas: 3000000 }),
+				3000000
+			)
+		})
+
+	})
+
+})
