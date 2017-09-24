86e1618aba51ee8b78660d8724f562b0be95368b
pospi <pospi@spadgos.com>
2017-09-24 22:16:10 +1000

    ensure pausable permissions applied correctly to its privileged methods

--- test/Pausable.js
+++ test/Pausable.js
@@ -20,6 +20,24 @@ contract('Pausable', (accounts) => {
 		owner1 = accounts[1]
 	})
 
+	describe("permissions", () => {
+
+		it("should allow changing paused status if the owner", async() => {
+			const paused = await Pausable.new(false, { from: owner0 })
+			await paused.setPaused(true, { from: owner0 })
+			assert.isTrue(await paused.isPaused())
+		})
+
+		it("should not allow changing paused status if not the owner", async() => {
+			const paused = await Pausable.new(false, { from: owner0 })
+			return expectedExceptionPromise(
+				() => paused.setPaused(true, { from: owner1, gas: 3000000 }),
+				3000000
+			)
+		})
+
+	})
+
 	describe("whenPaused", () => {
 
 		it("allows actions when paused", async() => {
