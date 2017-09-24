807601c50f67b4f331f83cb0a534b1627f6c581c
pospi <pospi@spadgos.com>
2017-09-24 19:27:54 +1000

    Pausable implementation

---
+++ contracts/Pausable.sol
@@ -0,0 +1,51 @@
+pragma solidity 0.4.15;
+
+import "./interfaces/PausableI.sol";
+import "./Owned.sol";
+
+
+contract Pausable is PausableI, Owned {
+    bool paused;
+
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+    function Pausable(bool isPaused) {
+        paused = isPaused;
+    }
+
+    /**
+     * Event emitted when a new paused state has been set.
+     * @param sender The account that ran the action.
+     * @param newPausedState The new, and current, paused state of the contract.
+     */
+    event LogPausedSet(address indexed sender, bool indexed newPausedState);
+
+    /**
+     * Sets the new paused state for this contract.
+     *     It should roll back if the caller is not the current owner of this contract.
+     *     It should roll back if the state passed is no different from the current.
+     * @param newState The new desired "paused" state of the contract.
+     * @return Whether the action was successful.
+     * Emits LogPausedSet.
+     */
+    function setPaused(bool newState) returns(bool success) {
+        paused = newState;
+        LogPausedSet(msg.sender, newState);
+        return true;
+    }
+
+    /**
+     * @return Whether the contract is indeed paused.
+     */
+    function isPaused() constant returns(bool isIndeed) {
+        return paused;
+    }
+}
