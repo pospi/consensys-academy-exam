cf48fa3eb0fa37e01b2d338437009bea4a28ab27
pospi <pospi@spadgos.com>
2017-09-24 23:27:42 +1000

    fix indentation

--- contracts/Pausable.sol
+++ contracts/Pausable.sol
@@ -5,48 +5,48 @@ import "./Owned.sol";
 
 
 contract Pausable is PausableI, Owned {
-    bool paused;
+	bool paused;
 
-    modifier whenPaused() {
-        require(paused);
-        _;
-    }
+	modifier whenPaused() {
+		require(paused);
+		_;
+	}
 
-    modifier whenNotPaused() {
-        require(!paused);
-        _;
-    }
+	modifier whenNotPaused() {
+		require(!paused);
+		_;
+	}
 
-    function Pausable(bool isPaused) Owned() {
-        paused = isPaused;
-    }
+	function Pausable(bool isPaused) Owned() {
+		paused = isPaused;
+	}
 
-    /**
-     * Event emitted when a new paused state has been set.
-     * @param sender The account that ran the action.
-     * @param newPausedState The new, and current, paused state of the contract.
-     */
-    event LogPausedSet(address indexed sender, bool indexed newPausedState);
+	/**
+	 * Event emitted when a new paused state has been set.
+	 * @param sender The account that ran the action.
+	 * @param newPausedState The new, and current, paused state of the contract.
+	 */
+	event LogPausedSet(address indexed sender, bool indexed newPausedState);
 
-    /**
-     * Sets the new paused state for this contract.
-     *     It should roll back if the caller is not the current owner of this contract.
-     *     It should roll back if the state passed is no different from the current.
-     * @param newState The new desired "paused" state of the contract.
-     * @return Whether the action was successful.
-     * Emits LogPausedSet.
-     */
-    function setPaused(bool newState) fromOwner() public returns(bool success) {
-    		require(newState != paused);
-        paused = newState;
-        LogPausedSet(msg.sender, newState);
-        return true;
-    }
+	/**
+	 * Sets the new paused state for this contract.
+	 *	 It should roll back if the caller is not the current owner of this contract.
+	 *	 It should roll back if the state passed is no different from the current.
+	 * @param newState The new desired "paused" state of the contract.
+	 * @return Whether the action was successful.
+	 * Emits LogPausedSet.
+	 */
+	function setPaused(bool newState) fromOwner() public returns(bool success) {
+		require(newState != paused);
+	  paused = newState;
+	  LogPausedSet(msg.sender, newState);
+	  return true;
+	}
 
-    /**
-     * @return Whether the contract is indeed paused.
-     */
-    function isPaused() constant public returns(bool isIndeed) {
-        return paused;
-    }
+	/**
+	 * @return Whether the contract is indeed paused.
+	 */
+	function isPaused() constant public returns(bool isIndeed) {
+		return paused;
+	}
 }
