76979c61c1eb74ba49284a7f975498d5373c351d
pospi <pospi@spadgos.com>
2017-09-24 19:29:07 +1000

    unit tests for Pausable

---
+++ test/TestPausable.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.4.15;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Pausable.sol";
+
+contract TestPausable {
+    function testDefaultPausable() {
+        PausableI test = new Pausable(true);
+        Assert.isTrue(test.isPaused(), "Failed to initialise paused");
+        PausableI test2 = new Pausable(false);
+        Assert.isFalse(test2.isPaused(), "Failed to initialise unpaused");
+    }
+
+    function testSetPausable() {
+        PausableI test = new Pausable(false);
+        Assert.isTrue(test.setPaused(true), "Failed to update paused status");
+        Assert.equal(test.isPaused(), true, "Paused status should have been changed");
+    }
+}
