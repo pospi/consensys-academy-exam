560820d297a573a8a9ef3fd2fe588b62fd9c3a73
pospi <pospi@spadgos.com>
2017-09-25 22:47:18 +1000

    RoutePriceHolder implementation

---
+++ contracts/RoutePriceHolder.sol
@@ -0,0 +1,88 @@
+/**
+ * Contract which stores the route prices
+ *
+ * @package: Consensys Academy exam pt2
+ * @author:  pospi <sam.pospi@consensys.net>
+ * @since:   2017-09-24
+ */
+pragma solidity 0.4.15;
+
+import "./interfaces/RoutePriceHolderI.sol";
+import "./TollBoothHolder.sol";
+
+
+contract RoutePriceHolder is RoutePriceHolderI, TollBoothHolder
+{
+	mapping(address => mapping(address => uint)) routePrices;
+
+	/**
+	 * Event emitted when a new price has been set on a route.
+	 * @param sender The account that ran the action.
+	 * @param entryBooth The address of the entry booth of the route set.
+	 * @param exitBooth The address of the exit booth of the route set.
+	 * @param priceWeis The price in weis of the new route.
+	 */
+	event LogRoutePriceSet(
+		address indexed sender,
+		address indexed entryBooth,
+		address indexed exitBooth,
+		uint priceWeis);
+
+	/**
+	 * Called by the owner of the RoutePriceHolder.
+	 *	 It can be used to update the price of a route, including to zero.
+	 *	 It should roll back if the caller is not the owner of the contract.
+	 *	 It should roll back if one of the booths is not a registered booth.
+	 *	 It should roll back if entry and exit booths are the same.
+	 *	 It should roll back if either booth is a 0x address.
+	 *	 It should roll back if there is no change in price.
+	 *	 If relevant, and only when part of TollBoothOperatorI, it will release 1 pending payment
+	 *	   for this route.
+	 *	 It should not roll back if the relevant pending payment is not solvable, if, for
+	 *	   instance the vehicle has had wrongly set values in the interim.
+	 *	 When part of TollBoothOperatorI, it should be possible to call it even when the contract
+	 *	   is in the `true` paused state.
+	 * @param entryBooth The address of the entry booth of the route set.
+	 * @param exitBooth The address of the exit booth of the route set.
+	 * @param priceWeis The price in weis of the new route.
+	 * @return Whether the action was successful.
+	 * Emits LogPriceSet.
+	 */
+	function setRoutePrice(
+		address entryBooth,
+		address exitBooth,
+		uint priceWeis
+	)
+		fromOwner()
+		public
+		returns(bool success)
+	{
+		require(entryBooth != exitBooth);
+		require(entryBooth != 0x0 && exitBooth != 0x0);
+		require(isTollBooth(entryBooth) && isTollBooth(exitBooth));
+		require(routePrices[entryBooth][exitBooth] != priceWeis);
+
+		routePrices[entryBooth][exitBooth] = priceWeis;
+
+		LogRoutePriceSet(msg.sender, entryBooth, exitBooth, priceWeis);
+
+		return true;
+	}
+
+	/**
+	 * @param entryBooth The address of the entry booth of the route.
+	 * @param exitBooth The address of the exit booth of the route.
+	 * @return priceWeis The price in weis of the route.
+	 *	 If the route is not known or if any address is not a booth it should return 0.
+	 *	 If the route is invalid, it should return 0.
+	 */
+	function getRoutePrice(
+		address entryBooth,
+		address exitBooth)
+		constant
+		public
+		returns(uint priceWeis)
+	{
+		return routePrices[entryBooth][exitBooth];
+	}
+}
